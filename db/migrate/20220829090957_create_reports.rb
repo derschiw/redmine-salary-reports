2eda057be5df5543478e007b4c6c117e76866eaa
derschiw <aeneas.meier@protonmail.com>
2022-08-29 11:13:21 +0200

    report model migrations

---
+++ db/migrate/20220829090957_create_reports.rb
@@ -0,0 +1,12 @@
+class CreateReports < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reports do |t|
+      t.references :user, foreign_key: true
+      t.datetime :date_from
+      t.datetime :date_to
+      t.decimal :overtime_ignoring_holidays
+      t.decimal :overtime_considering_holidays
+      t.decimal :holidays_taken
+    end
+  end
+end
